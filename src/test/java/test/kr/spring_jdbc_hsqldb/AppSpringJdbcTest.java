d069360e344ada56024a6c2f70663ee0dc4bf204
sanovice <sanovice@gmail.com>
2013-10-30 19:14:40 +0900

    simplejdbctemplate 테스트추가.

--- src/test/java/test/kr/spring_jdbc_hsqldb/AppSpringJdbcTest.java
+++ src/test/java/test/kr/spring_jdbc_hsqldb/AppSpringJdbcTest.java
@@ -2,12 +2,18 @@ package test.kr.spring_jdbc_hsqldb;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -26,9 +32,25 @@ public class AppSpringJdbcTest {
 	
 	
 	@Test
-	public void test() {
+	public void testSimpleJdbc() {
 		assertNotNull(app); 
 		assertNotNull(dataSource); 
+		
+		
+		SimpleJdbcTemplate simple = new SimpleJdbcTemplate(dataSource); 
+		
+		String sql = "select * from Product "; 
+		
+		Map<String, ?> args = new HashMap<String, Object>(); 
+		
+		
+		List results = new ArrayList(); 
+		results = simple.queryForList(sql, args);
+		
+		for(Object result : results) {
+			System.out.println("result=" + result); 
+		}
+		
 		
 	}
 
