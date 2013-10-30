3a2a0bcc2fd33f8f879f47be2b55ae36a31abf5d
sanovice <sanovice@gmail.com>
2013-10-30 17:09:56 +0900

    init maven project create

---
+++ src/main/java/test/kr/spring_jdbc_hsqldb/App.java
@@ -0,0 +1,13 @@
+package test.kr.spring_jdbc_hsqldb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
