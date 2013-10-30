85cfd575b3b651ba8f34d4eea854413f92ccd2dd
GitTester1 <GitTester@domain.com>
2013-10-31 02:46:01 +0900

    sample data select and insert other table test

--- src/test/java/test/kr/spring_jdbc_hsqldb/AppSpringJdbcTest.java
+++ src/test/java/test/kr/spring_jdbc_hsqldb/AppSpringJdbcTest.java
@@ -14,8 +14,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.test.jdbc.SimpleJdbcTestUtils;
 
 
 @ContextConfiguration({"/config/app-root-test.xml", "/config/app-datasource-test.xml"})
@@ -44,14 +47,28 @@ public class AppSpringJdbcTest {
 		Map<String, ?> args = new HashMap<String, Object>(); 
 		
 		
-		List results = new ArrayList(); 
+		List<?> results = new ArrayList<Object>(); 
 		results = simple.queryForList(sql, args);
 		
 		for(Object result : results) {
-			System.out.println("result=" + result); 
+			System.out.println("type=" + result.getClass() + " value=" + result ); 
 		}
 		
+		// dest insert 
+		//CREATE TABLE Product2(ID INTEGER PRIMARY KEY,Name VARCHAR(30),Price DECIMAL);
+		String sql_insert = "INSERT INTO Product2 VALUES(:ID, :NAME, :PRICE)";
 		
+		for(Object result : results) {
+			simple.update(sql_insert, (Map<String, ?>)result); 
+		}
+		
+		// dest result 
+		List<?> rs2 = new ArrayList<Object>();
+		rs2 = simple.queryForList("select * from Product2", args);
+		System.out.println(rs2);
+		
+		
+		System.out.println("count=" + SimpleJdbcTestUtils.countRowsInTable(simple, "Product2"));
 	}
 
 }
